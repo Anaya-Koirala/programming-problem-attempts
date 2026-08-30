db3fccd06b83edafd37fcb71d007d194a6cb432b
Anaya Koirala <anaya@koirala.xyz>
2026-08-30 11:42:26 -0400

    Update and rename biladlyklabord.java to BiladlyKlabord.java

--- BiladlyKlabord.java
+++ BiladlyKlabord.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 
-public class biladlyklabord {
+public class BiladlyKlabord {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         StringBuilder result = new StringBuilder();
