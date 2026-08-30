efcc49d5c6e33fd8b13e3647825a0be02354d34e
Anaya Koirala <anaya@koirala.xyz>
2026-08-30 11:39:11 -0400

    Create RandomizedSet.java

---
+++ RandomizedSet.java
@@ -0,0 +1,47 @@
+class RandomizedSet {
+    // K -> Val, V -> idx 
+    private Map<Integer, Integer> map;
+    private ArrayList<Integer> list;
+    private Random rnd;
+    private int idx;
+
+    public RandomizedSet() {
+        list = new ArrayList<>();
+        map = new HashMap<>();
+        rnd = new Random();
+        idx = 0;
+    }
+    
+    public boolean insert(int val) {
+        if (map.containsKey(val)) {
+            return false;
+        }
+        map.put(val, idx);
+        list.add(val);
+        idx += 1;
+        return true;
+    }
+    
+    public boolean remove(int val) {
+        if (!map.containsKey(val)) {
+            return false;
+        }
+        
+        int valIdx = map.get(val);
+        int lastElem = list.get(idx - 1);
+        
+        list.set(valIdx, lastElem);
+        map.put(lastElem, valIdx);
+        
+        list.remove(idx - 1);
+        idx -= 1;
+        map.remove(val);
+        
+        return true;
+    }
+    
+    public int getRandom() {
+        int randomIdx = rnd.nextInt(idx);
+        return list.get(randomIdx);
+    }
+}
